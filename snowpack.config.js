0c0217eeaa4aac2ee130b4d70b55f7d07c15a444
Damilola Randolph <damilolarandolph@gmail.com>
2021-05-28 01:59:49 +0000

    Add tailwind css.

--- snowpack.config.js
+++ snowpack.config.js
@@ -3,5 +3,11 @@ module.exports = {
         public: { url: "/", static: true },
         src: "/",
         wasm: { url: "/wasm", static: true }
-    }
+    },
+    devOptions: {
+        tailwindConfig: './tailwind.config.js',
+    },
+    plugins: [
+        '@snowpack/plugin-postcss',
+    ],
 }
