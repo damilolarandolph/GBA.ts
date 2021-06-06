1fb9f6aa9176f5f7d83d1db3b6277a14562303a5
Damilola Randolph <damilolarandolph@gmail.com>
2021-06-06 02:34:02 +0000

    Added remaining opcode classifiers.

--- assembly/cpu/instructions/arm/arm.ts
+++ assembly/cpu/instructions/arm/arm.ts
@@ -29,3 +29,36 @@ export function isMultiplyLong(miniOpMSB: u8, miniOpLSB: u8): bool {
 export function isSingleDataSwap(miniOpMSB: u8, miniOpLSB: u8): bool {
     return matchBitPattern("00010x00", miniOpMSB) && matchBitPattern("1001", miniOpLSB);
 }
+
+export function isBranchExchange(miniOpMSB: u8, miniOpLSB: u8): bool {
+    return matchBitPattern("00010010", miniOpMSB) && matchBitPattern("0001", miniOpLSB);
+}
+
+export function isHalfwordDataTransferRegOff(miniOpMSB: u8, miniOpLSB: u8): bool {
+    let match = matchBitPattern("000xx0xx", miniOpMSB) && matchBitPattern("1xx1", miniOpLSB);
+    return match;
+}
+
+export function isHalfwordDataTransferImmediateOff(miniOpMSB: u8, miniOpLSB: u8): bool {
+    return matchBitPattern("000xx1xx", miniOpMSB) && matchBitPattern("1xx1", miniOpLSB);
+}
+
+export function isSingleDataTransfer(miniOpMSB: u8, _miniOpLSB: u8): bool {
+    return matchBitPattern("01xxxxxx", miniOpMSB);
+}
+
+export function undefined(miniOpMSB: u8, miniOpLSB: u8): bool {
+    return matchBitPattern("011xxxxx", miniOpMSB) && matchBitPattern("xxx1", miniOpLSB);
+}
+
+export function isBlockDataTransfer(miniOpMSB: u8, _miniOpLSB: u8): bool {
+    return matchBitPattern("100xxxxx", miniOpMSB);
+}
+
+export function isBranch(miniOpMSB: u8, _miniOpLSB: u8): bool {
+    return matchBitPattern("101xxxxx", miniOpMSB);
+}
+
+export function isSoftwareInterrupt(miniOpMSB: u8, _miniOpLSB: u8): bool {
+    return matchBitPattern("11111111", miniOpMSB);
+}
