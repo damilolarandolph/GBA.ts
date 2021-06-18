eb93119b577285fc7b52cce1dc60a3f2e882022a
Damilola Randolph <damilolarandolph@gmail.com>
2021-06-18 20:12:13 +0000

    Fix opcode population logic, added logging.

--- assembly/cpu/instructions/arm/arm.ts
+++ assembly/cpu/instructions/arm/arm.ts
@@ -1,4 +1,4 @@
-import { matchBitPattern } from '../../../utils/bits';
+import { getBit, getBits, matchBitPattern } from '../../../utils/bits';
 
 export * from './address-modes';
 export * from './alu';
@@ -10,12 +10,48 @@ export * from './multiply';
 
 export function isDataProcessingOrPSR(miniOpMSB: u8, miniOpLSB: u8): bool {
     let result = matchBitPattern("00xxxxxx", miniOpMSB);
-    let isImmediate = ((miniOpMSB >>> 5) & 0x1) != 0;
-    if (isImmediate && (miniOpMSB & 0x1) != 0)
+    if (!result)
         return false;
-    if (!isImmediate && (miniOpLSB & 0x8) != 0)
+    let fullInstruction: u32 = (u32(miniOpMSB) << 20) | (u32(miniOpLSB) << 4);
+    let opcode = getBits(fullInstruction, 24, 21);
+    let sConds = getBit(fullInstruction, 20);
+    let iBit = getBit(fullInstruction, 25);
+    let rBit = getBit(fullInstruction, 4);
+    let shiftType = getBits(fullInstruction, 6, 5);
+    if (opcode > 0xf)
         return false;
+
+    if (!sConds && (opcode >= 0x8 && opcode <= 0xb))
+        return false;
+
+    if (!iBit && rBit && getBit(fullInstruction, 4))
+        return false;
+
+    if (!iBit && (shiftType < 0 || shiftType > 3)) {
+        return false;
+    }
+
+
     return result;
+}
+
+export function isPSR(miniOpMSB: u8, miniOpLSB: u8): bool {
+    return matchBitPattern("00010xx0", miniOpMSB) && matchBitPattern("0000", miniOpLSB);
+}
+
+export function isPSRImm(miniOpMSB: u8, miniOpLSB: u8): bool {
+    let res = matchBitPattern("00x10x10", miniOpMSB);
+    if (!res) {
+        return false;
+    }
+
+    let fullInstruction: u32 = (u32(miniOpMSB) << 20) | (u32(miniOpLSB) << 4);
+    let iBit = getBit(fullInstruction, 25);
+    if (!iBit && getBits(fullInstruction, 7, 4) != 0) {
+        return false;
+    }
+
+    return true;
 }
 
 export function isMultiply(miniOpMSB: u8, miniOpLSB: u8): bool {
@@ -49,6 +85,7 @@ export function isSingleDataTransfer(miniOpMSB: u8, _miniOpLSB: u8): bool {
 
 export function undefined(miniOpMSB: u8, miniOpLSB: u8): bool {
     return matchBitPattern("011xxxxx", miniOpMSB) && matchBitPattern("xxx1", miniOpLSB);
+
 }
 
 export function isBlockDataTransfer(miniOpMSB: u8, _miniOpLSB: u8): bool {
--- assembly/cpu/instructions/initialize.ts
+++ assembly/cpu/instructions/initialize.ts
@@ -3,6 +3,7 @@ import { ARM7CPU } from '../cpu';
 import * as ARM from './arm/arm';
 import { LDM, LDM2, STM, STM2 } from './arm/arm';
 export type opHandler = (cpu: ARM7CPU) => void;
+import { console } from '../../bridge';
 
 export var armOpTable: StaticArray<opHandler | null> = new StaticArray(4096);
 
@@ -10,103 +11,145 @@ export function initARM(): void {
 
     for (let row = 0; row < 256; row++) {
         for (let col = 0; col < 16; col++) {
-            let fullInstruction: u32 = (row << 24) & col;
-            let arrIndex = row * col;
-            if (ARM.isDataProcessingOrPSR(<u8>row, <u8>col)) {
+            let fullInstruction: u32 = (u32(row) << 20) | (u32(col) << 4);
+            let arrIndex = row * 16 + col;
+
+            if (ARM.isPSR(<u8>row, <u8>col) || ARM.isPSRImm(<u8>row, <u8>col)) {
+                let mrsBit = getBit(fullInstruction, 21);
+                if (!mrsBit) {
+                    unchecked(armOpTable[arrIndex] = ARM.MRS);
+                    console.log(`MRS row ${row.toString(16)} col ${col.toString(16)}`);
+                } else {
+                    unchecked(armOpTable[arrIndex] = ARM.MSR);
+                    console.log(`MSR row ${row.toString(16)} col ${col.toString(16)}`);
+                }
+            }
+            else if (ARM.isDataProcessingOrPSR(<u8>row, <u8>col)) {
                 let opcode = (row >>> 1) & 0xf;
-                let opFull = row * col;
+                let opFull = row * 16 + col;
                 switch (opcode) {
                     case 0b0000:
                         unchecked(armOpTable[opFull] = ARM.AND);
+                        console.log(`AND row ${row.toString(16)} col ${col.toString(16)}`);
                         break;
                     case 0b0001:
                         unchecked(armOpTable[opFull] = ARM.EOR);
+                        console.log(`EOR row ${row.toString(16)} col ${col.toString(16)}`);
                         break;
                     case 0b0010:
                         unchecked(armOpTable[opFull] = ARM.SUB);
+                        console.log(`SUB row ${row.toString(16)} col ${col.toString(16)}`);
                         break;
                     case 0b0011:
                         unchecked(armOpTable[opFull] = ARM.RSB);
+                        console.log(`RSB row ${row.toString(16)} col ${col.toString(16)}`);
                         break;
                     case 0b0100:
                         unchecked(armOpTable[opFull] = ARM.ADD);
+                        console.log(`ADD row ${row.toString(16)} col ${col.toString(16)}`);
                         break;
                     case 0b0101:
                         unchecked(armOpTable[opFull] = ARM.ADDC);
+                        console.log(`ADDC row ${row.toString(16)} col ${col.toString(16)}`);
                         break;
                     case 0b0110:
                         unchecked(armOpTable[opFull] = ARM.SBC);
+                        console.log(`SBC row ${row.toString(16)} col ${col.toString(16)}`);
                         break;
                     case 0b0111:
                         unchecked(armOpTable[opFull] = ARM.RSC);
+                        console.log(`RSC row ${row.toString(16)} col ${col.toString(16)}`);
                         break;
                     case 0b1000:
                         unchecked(armOpTable[opFull] = ARM.TST);
+                        console.log(`TST row ${row.toString(16)} col ${col.toString(16)}`);
                         break;
                     case 0b1001:
                         unchecked(armOpTable[opFull] = ARM.TEQ);
+                        console.log(`TEQ row ${row.toString(16)} col ${col.toString(16)}`);
                         break;
                     case 0b1010:
                         unchecked(armOpTable[opFull] = ARM.CMP);
+                        console.log(`CMP row ${row.toString(16)} col ${col.toString(16)}`);
                         break;
                     case 0b1011:
                         unchecked(armOpTable[opFull] = ARM.CMN);
+                        console.log(`CMN row ${row.toString(16)} col ${col.toString(16)}`);
                         break;
                     case 0b1100:
                         unchecked(armOpTable[opFull] = ARM.ORR);
+                        console.log(`ORR row ${row.toString(16)} col ${col.toString(16)}`);
                         break;
                     case 0b1101:
-                        unchecked(armOpTable[opFull] = ARM.MOV);
+                        armOpTable[opFull] = ARM.MOV;
+                        console.log(`MOV row ${row.toString(16)} col ${col.toString(16)}`);
                         break;
                     case 0b1110:
                         unchecked(armOpTable[opFull] = ARM.BIC);
+                        console.log(`BIC row ${row.toString(16)} col ${col.toString(16)}`);
                         break;
                     case 0b1111:
                         unchecked(armOpTable[opFull] = ARM.MVN);
+                        console.log(`MVN row ${row.toString(16)} col ${col.toString(16)}`);
                         break;
                 }
-                continue
             }
 
 
-            //Multiply
+            //            Multiply
 
             else if (ARM.isMultiply(<u8>row, <u8>col)) {
                 if (getBit(fullInstruction, 21)) {
+                    console.log(`MLA row ${row.toString(16)} col ${col.toString(16)}`);
                     armOpTable[arrIndex] = ARM.MLA
                 } else {
+                    console.log(`MUL row ${row.toString(16)} col ${col.toString(16)}`);
                     armOpTable[arrIndex] = ARM.MUL
                 }
             }
 
-            //Multiply Accumulate
+            // //Multiply Accumulate
 
             else if (ARM.isMultiplyLong(<u8>row, <u8>col)) {
                 let uBit = getBit(fullInstruction, 22);
                 let aBit = getBit(fullInstruction, 21);
                 if (aBit) {
-                    if (uBit)
+                    if (uBit) {
                         armOpTable[arrIndex] = ARM.SMLAL;
-                    else
+                        console.log(`SMLAL row ${row.toString(16)} col ${col.toString(16)}`);
+                    }
+                    else {
+
+                        console.log(`UMLAL row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.UMLAL;
+                    }
                 }
                 else {
-                    if (uBit)
+                    if (uBit) {
                         armOpTable[arrIndex] = ARM.SMLUL;
-                    else
+                        console.log(`SMLUL row ${row.toString(16)} col ${col.toString(16)}`);
+                    }
+                    else {
+                        console.log(`UMULL row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.UMULL;
+                    }
                 }
             }
 
             else if (ARM.isSingleDataSwap(<u8>row, <u8>col)) {
                 if (getBit(fullInstruction, 22)) {
+
+                    console.log(`SWPB row ${row.toString(16)} col ${col.toString(16)}`);
                     armOpTable[arrIndex] = ARM.SWPB;
                 } else {
+                    console.log(`SWP row ${row.toString(16)} col ${col.toString(16)}`);
                     armOpTable[arrIndex] = ARM.SWP;
                 }
             }
 
             else if (ARM.isBranchExchange(<u8>row, <u8>col)) {
+
+                console.log(`BX row ${row.toString(16)} col ${col.toString(16)}`);
                 armOpTable[arrIndex] = ARM.BX;
             }
 
@@ -115,15 +158,23 @@ export function initARM(): void {
                 let type = getBits(fullInstruction, 6, 5);
                 if (lbit) {
                     if (type == 0b01) {
+
+                        console.log(`LDRH row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.LDRH;
                     } else if (type == 0b10) {
+
+                        console.log(`LDRSB row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.LDRSB;
                     } else {
+
+                        console.log(`LDRSH row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.LDRSH;
                     }
                 } else {
-                    if (type == 1)
+                    if (type == 1) {
+                        console.log(`STRH row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.STRH;
+                    }
                 }
             }
 
@@ -132,16 +183,25 @@ export function initARM(): void {
                 let type = getBits(fullInstruction, 6, 5);
                 if (lbit) {
                     if (type == 0b01) {
+
+                        console.log(`LDRH row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.LDRH;
                     } else if (type == 0b10) {
+
+                        console.log(`LDRSB row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.LDRSB;
                     } else {
+
+                        console.log(`LDRSH row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.LDRSH;
                     }
                 } else {
-                    if (type == 1)
+                    if (type == 1) {
+                        console.log(`STRH row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.STRH;
+                    }
                 }
+
 
             }
 
@@ -151,23 +211,41 @@ export function initARM(): void {
                 let lbit = getBit(fullInstruction, 20);
                 let bBit = getBit(fullInstruction, 22);
                 if (lbit) {
-                    if (!pBit && tBit && bBit)
+                    if (!pBit && tBit && bBit) {
+
+                        console.log(`LDRBT row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.LDRBT;
-                    else if (!pBit && tBit)
+                    }
+                    else if (!pBit && tBit) {
+                        console.log(`LDRT row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.LDRT;
-                    else if (bBit)
+                    }
+                    else if (bBit) {
+                        console.log(`LDRB row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.LDRB;
-                    else
+                    }
+                    else {
+                        console.log(`LDR row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.LDR;
+                    }
                 } else {
-                    if (!pBit && tBit && bBit)
+                    if (!pBit && tBit && bBit) {
+                        console.log(`STRBT row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.STRBT;
-                    else if (!pBit && tBit)
+                    }
+                    else if (!pBit && tBit) {
+                        console.log(`STRT row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.STRT
-                    else if (bBit)
+                    }
+                    else if (bBit) {
+                        console.log(`STRB row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.STRB;
-                    else
+                    }
+                    else {
+
+                        console.log(`STR row ${row.toString(16)} col ${col.toString(16)}`);
                         armOpTable[arrIndex] = ARM.STR;
+                    }
 
                 }
             }
@@ -179,19 +257,30 @@ export function initARM(): void {
             else if (ARM.isBlockDataTransfer(<u8>row, <u8>col)) {
                 let lbit = getBit(fullInstruction, 20);
                 let sBit = getBit(fullInstruction, 22);
-                if (lbit && sBit)
+                if (lbit && sBit) {
+                    console.log(`LDM2 row ${row.toString(16)} col ${col.toString(16)}`);
                     armOpTable[arrIndex] = LDM2;
-                else if (lbit)
+                }
+                else if (lbit) {
+                    console.log(`LDM row ${row.toString(16)} col ${col.toString(16)}`);
                     armOpTable[arrIndex] = LDM;
-                else if (!lbit && sBit)
+                }
+                else if (!lbit && sBit) {
+                    console.log(`STM row ${row.toString(16)} col ${col.toString(16)}`);
                     armOpTable[arrIndex] = STM;
-                else if (!lbit)
+                }
+                else if (!lbit) {
+                    console.log(`STM2 row ${row.toString(16)} col ${col.toString(16)}`);
                     armOpTable[arrIndex] = STM2;
+                }
             }
 
             else if (ARM.isBranch(<u8>row, <u8>col)) {
+                console.log(`BBL row ${row.toString(16)} col ${col.toString(16)}`);
                 armOpTable[arrIndex] = ARM.BBL;
-            } else if (ARM.isSoftwareInterrupt(<u8>row, <u8>col)) {
+            }
+            else if (ARM.isSoftwareInterrupt(<u8>row, <u8>col)) {
+                console.log(`SWI row ${row.toString(16)} col ${col.toString(16)}`);
                 armOpTable[arrIndex] = ARM.SWI;
             }
 
