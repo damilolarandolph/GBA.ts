bd50a48daf23da8a85cbc0809b0c60c02237f0ed
Damilola Randolph <damilolarandolph@gmail.com>
2021-06-19 21:56:26 +0000

    Dummy interface.

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,24 @@
-// The entry file of your WebAssembly module.
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
+import { cartData } from "./gamepak/gamepak";
+import GBA from "./gba";
+
+var gba: GBA = new GBA();
+
+
+
+export function getCartData(): Uint8Array {
+
+  return cartData;
 }
+
+export function run(): void {
+  gba.run();
+}
+
+export function getGBA(): GBA {
+  return gba;
+}
+
+
+
+
