b786c47cab7f36c6c6d4eeb4dfc06ac3d7df5051
Damilola Randolph <damilolarandolph@gmail.com>
2021-06-19 21:55:25 +0000

    Fill interrupt manager logic.

--- assembly/cpu/interrupt-manager.ts
+++ assembly/cpu/interrupt-manager.ts
@@ -1,4 +1,7 @@
-import MemoryBlock from "../memory/memory-block";
+import IODevice from "../io/io-device";
+import MemoryMap from "../memory/memory-map";
+import { getBit, setBit } from "../utils/bits";
+import { ARM7CPU, CPU_MODES } from "./cpu";
 
 
 enum INTERRUPTS {
@@ -19,12 +22,56 @@ enum INTERRUPTS {
 }
 
 
-export default class InterruptManager implements MemoryBlock {
+export default class InterruptManager implements IODevice {
+    private ime: u32 = 0;
+    private ie: u16 = 0;
+    private if: u16 = 0;
 
 
+    handleInterrupts(cpu: ARM7CPU): void {
+        cpu.writeRegister(15, 0x00000128);
+    }
 
 
+    public requestInterrupt(interrupt: INTERRUPTS): void {
+        this.if = setBit(this.if, interrupt, true);
+    }
+
+    public hasServicableInterrupts(): bool {
+        return this.interruptsEnabled() && (this.ie & this.if) != 0
+    }
+
+    public enableInterrupt(interrupt: INTERRUPTS): void {
+        this.ie = setBit(this.ie, interrupt, true);
+    }
+
+    public disableInterrupt(interrupt: INTERRUPTS): void {
+        this.ie = setBit(this.ie, interrupt, false);
+    }
+
+    public unrequestInterrupt(interrupt: INTERRUPTS): void {
+        this.if = setBit(this.if, interrupt, false);
+    }
 
 
+    public interruptsEnabled(): bool {
+        return this.ime == 0;
+    }
+
+    public enableInterrupts(): void {
+        this.ime = 1;
+    }
+
+    public disableInterrupts(): void {
+        this.ime = 0;
+    }
+
+
+    writeIO(address: u32, value: u32): void {
+        throw new Error("Method not implemented.");
+    }
+    readIO(address: u32): u32 {
+        throw new Error("Method not implemented.");
+    }
 
 }
