b04d334d0dd18b28150d71ae84a001dc75887f18
Damilola Randolph <damilolarandolph@gmail.com>
2021-07-07 03:15:44 +0000

    Clean up branch instructions.

--- assembly/cpu/instructions/arm/branch.ts
+++ assembly/cpu/instructions/arm/branch.ts
@@ -3,10 +3,6 @@ import { ARM7CPU, StatusFlags } from "../../cpu";
 import { testCondition } from "../instructions";
 
 export function BBL(cpu: ARM7CPU): void {
-    if (!testCondition(cpu)) {
-        cpu.finish();
-        return;
-    }
 
     let instruction = cpu.currentInstruction;
     let lBit = getBit(instruction, 24);
@@ -30,20 +26,14 @@ export function BBL(cpu: ARM7CPU): void {
     }
 
     cpu.writeRegister(15, addr);
-    cpu.finish();
 
 }
 
 
 export function BX(cpu: ARM7CPU): void {
-    if (!testCondition(cpu)) {
-        cpu.finish();
-        return;
-    }
     let instruction = cpu.currentInstruction;
     let rm = getBits(instruction, 3, 0);
     let rmVal = cpu.readRegister(rm);
     cpu.setFlag(StatusFlags.THUMB_MODE, getBit(rmVal, 0));
     cpu.PC = rmVal & 0xFFFFFFFE;
-    cpu.finish();
 }
