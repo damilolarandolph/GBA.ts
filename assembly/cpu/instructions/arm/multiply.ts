8a3513ae0dbffbda397cc0be77a7ab0791defd6d
Damilola Randolph <damilolarandolph@gmail.com>
2021-07-13 05:52:11 +0000

    Fix multiplication instructions, passing multiply ARM FuzzARM 🚀

--- assembly/cpu/instructions/arm/multiply.ts
+++ assembly/cpu/instructions/arm/multiply.ts
@@ -64,19 +64,23 @@ export function UMLAL(cpu: ARM7CPU): void {
 
     let rdHi = getBits(cpu.currentInstruction, 19, 16);
     let rdLo = getBits(cpu.currentInstruction, 15, 12);
+    let hiVal = u64(cpu.readRegister(rdHi));
+    let loVal = u64(cpu.readRegister(rdLo));
     let rs = getBits(cpu.currentInstruction, 11, 8);
+    let rsVal = u64(cpu.readRegister(rs));
     let rm = getBits(cpu.currentInstruction, 3, 0);
+    let rmVal = u64(cpu.readRegister(rm));
     let sBit = getBit(cpu.currentInstruction, 20);
 
-    cpu.addCycles(deduceMStates(cpu.readRegister(rs)) + 2);
+    cpu.addCycles(deduceMStates(<u32>rsVal) + 2);
 
-    let hiLo: u64 = u64((u64(rdHi) << 32) | (u64(rdLo)));
-    let result: u64 = (u64(cpu.readRegister(rm)) * u64(cpu.readRegister(rs))) + hiLo;
-    cpu.writeRegister(rdHi, u32(result >> 32));
+    let hiLo: u64 = (hiVal << 32) | loVal;
+    let result: u64 = (rmVal * rsVal) + hiLo;
+    cpu.writeRegister(rdHi, u32(result >>> 32));
     cpu.writeRegister(rdLo, u32(result));
 
     if (sBit) {
-        cpu.setFlag(StatusFlags.NEGATIVE, getBit(u32(result >> 32), 31))
+        cpu.setFlag(StatusFlags.NEGATIVE, getBit(u32(result >>> 32), 31))
         cpu.setFlag(StatusFlags.ZERO, result == 0);
     }
 }
@@ -85,15 +89,26 @@ export function SMLAL(cpu: ARM7CPU): void {
 
     let rdHi = getBits(cpu.currentInstruction, 19, 16);
     let rdLo = getBits(cpu.currentInstruction, 15, 12);
+    let hiVal = i64(cpu.readRegister(rdHi));
+    let loVal = i64(cpu.readRegister(rdLo));
     let rs = getBits(cpu.currentInstruction, 11, 8);
     let rm = getBits(cpu.currentInstruction, 3, 0);
+    let rsVal = i64(cpu.readRegister(rs));
+    let rmVal = i64(cpu.readRegister(rm));
     let sBit = getBit(cpu.currentInstruction, 20);
 
-    cpu.addCycles(deduceMStates(cpu.readRegister(rs)) + 2);
+    cpu.addCycles(deduceMStates(u32(rsVal)) + 2);
+    // Simple Sign extension
+    if ((rsVal & 0x80000000) != 0) {
+        rsVal |= 0xFFFFFFFF00000000;
+    }
+    if ((rmVal & 0x80000000) != 0) {
+        rmVal |= 0xFFFFFFFF00000000;
+    }
 
-    let hiLo: i64 = i64((u64(rdHi) << 32) | (u64(rdLo)));
-    let result: i64 = (i64(cpu.readRegister(rm)) * i64(cpu.readRegister(rs))) + hiLo;
-    cpu.writeRegister(rdHi, u32(result >> 32));
+    let hiLo: i64 = (hiVal << 32) | loVal;
+    let result: i64 = (rsVal * rmVal) + hiLo;
+    cpu.writeRegister(rdHi, u32(result >>> 32));
     cpu.writeRegister(rdLo, u32(result));
 
     if (sBit) {
@@ -107,13 +122,25 @@ export function SMLUL(cpu: ARM7CPU): void {
     let rdHi = getBits(cpu.currentInstruction, 19, 16);
     let rdLo = getBits(cpu.currentInstruction, 15, 12);
     let rs = getBits(cpu.currentInstruction, 11, 8);
+    let rsVal = cpu.readRegister(rs);
     let rm = getBits(cpu.currentInstruction, 3, 0);
+    let rmVal = cpu.readRegister(rm);
     let sBit = getBit(cpu.currentInstruction, 20);
 
-    cpu.addCycles(deduceMStates(cpu.readRegister(rs)) + 1);
+    cpu.addCycles(deduceMStates(rsVal) + 1);
+    let rs64Val = i64(rsVal);
+    let rm64Val = i64(rmVal);
 
-    let result: i64 = (i64(cpu.readRegister(rm)) * i64(cpu.readRegister(rs)));
-    cpu.writeRegister(rdHi, u32(result >> 32));
+    // Simple Sign extension
+    if ((rs64Val & 0x80000000) != 0) {
+        rs64Val |= 0xFFFFFFFF00000000;
+    }
+    if ((rm64Val & 0x80000000) != 0) {
+        rm64Val |= 0xFFFFFFFF00000000;
+    }
+
+    let result = rm64Val * rs64Val;
+    cpu.writeRegister(rdHi, u32(result >>> 32));
     cpu.writeRegister(rdLo, u32(result));
 
     if (sBit) {
