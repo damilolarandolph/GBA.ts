6874bd50162990a19f49d8af74c8a6c2f7ebef97
Damilola Randolph <damilolarandolph@gmail.com>
2021-07-29 20:28:24 +0000

    Update enum name

--- assembly/cpu/interrupt-manager.ts
+++ assembly/cpu/interrupt-manager.ts
@@ -1,9 +1,10 @@
 import IODevice from "../io/io-device";
+import { Scheduler } from "../scheduler";
 import { getBit, setBit } from "../utils/bits";
 import { ARM7CPU, CPU_MODES } from "./cpu";
 
 
-enum INTERRUPTS {
+export enum Interrupts {
     VBLANK,
     HBLANK,
     COUNTERMATCH,
@@ -21,34 +22,38 @@ enum INTERRUPTS {
 }
 
 
-export default class InterruptManager implements IODevice {
+export class InterruptManager implements IODevice {
     private ime: u32 = 0;
     private ie: u16 = 0;
     private if: u16 = 0;
+    private scheduler: Scheduler;
 
+    constructor(scheduler: Scheduler) {
+        this.scheduler = scheduler;
+    }
 
     handleInterrupts(cpu: ARM7CPU): void {
         cpu.writeRegister(15, 0x00000128);
     }
 
 
-    public requestInterrupt(interrupt: INTERRUPTS): void {
-        this.if = setBit(this.if, interrupt, true);
+    public requestInterrupt(interrupt: Interrupts): void {
+        this.if = <u16>setBit(this.if, interrupt, true);
     }
 
     public hasServicableInterrupts(): bool {
         return this.interruptsEnabled() && (this.ie & this.if) != 0
     }
 
-    public enableInterrupt(interrupt: INTERRUPTS): void {
+    public enableInterrupt(interrupt: Interrupts): void {
         this.ie = setBit(this.ie, interrupt, true);
     }
 
-    public disableInterrupt(interrupt: INTERRUPTS): void {
+    public disableInterrupt(interrupt: Interrupts): void {
         this.ie = setBit(this.ie, interrupt, false);
     }
 
-    public unrequestInterrupt(interrupt: INTERRUPTS): void {
+    public unrequestInterrupt(interrupt: Interrupts): void {
         this.if = setBit(this.if, interrupt, false);
     }
 
