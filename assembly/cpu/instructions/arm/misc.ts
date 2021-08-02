9be335bb46d5cb3198b662706377c8dc5cc46d8c
Damilola Randolph <damilolarandolph@gmail.com>
2021-08-02 22:14:16 +0000

    Change branch names and add SWI.

--- assembly/cpu/instructions/arm/misc.ts
+++ assembly/cpu/instructions/arm/misc.ts
@@ -1,11 +1,6 @@
 import { ARM7CPU } from "../../cpu";
-import { testCondition } from "../instructions";
 
 
 export function SWI(cpu: ARM7CPU): void {
-    if (cpu.instructionStage == 0) {
-        if (!testCondition(cpu)) { }
-    }
     //TODO implement interrupts
-    cpu.finish();
 }
--- assembly/cpu/instructions/thumb/branch.ts
+++ assembly/cpu/instructions/thumb/branch.ts
@@ -4,7 +4,7 @@ import { ARM7CPU, StatusFlags } from "../../cpu";
 import { testCondition } from "../instructions";
 
 
-export function branchConditinal(cpu: ARM7CPU) {
+export function BC(cpu: ARM7CPU) {
 
     if (!testCondition(cpu)) {
         cpu.addCycles(1);
@@ -29,7 +29,7 @@ export function branchConditinal(cpu: ARM7CPU) {
 }
 
 
-export function branchUnconditional(cpu: ARM7CPU) {
+export function B(cpu: ARM7CPU) {
     let instruction = cpu.currentInstruction;
     let targetAddr: u32 = instruction & 0x7FF;
 
@@ -70,7 +70,8 @@ export function BLBLX(cpu: ARM7CPU) {
 
 export function BX(cpu: ARM7CPU) {
     let instruction = cpu.currentInstruction;
-    let rm = (instruction >> 3) & 0x7;
+    let h2Bit = (instruction >>> 6) & 0x1;
+    let rm = (h2Bit << 4) | ((instruction >> 3) & 0x7);
     let rmVal = cpu.readRegister(rm);
     cpu.setFlag(StatusFlags.THUMB_MODE, (rmVal & 0x1) != 0);
     cpu.writeRegister(15, rmVal & 0xFFFFFFFE);
