4d5286dfd34b2f2cb677936f4524baf80bb4bd1b
Damilola Randolph <damilolarandolph@gmail.com>
2021-08-08 21:39:31 +0000

    Remove generated files entirely.

--- assembly/cpu/instructions/arm-op-table.ts
+++
@@ -1,8 +0,0 @@
-
-import { ARM7CPU } from '../cpu';
-import * as arm from './arm/arm';
-import { opHandler } from "./instructions";
-
-
-// If you are reading this, you haven't generated the ARM opcode table.
-export const armOpTable: StaticArray<StaticArray<opHandler | null>> = []
--- assembly/cpu/instructions/thumb-op-table.ts
+++
@@ -1,9 +0,0 @@
-
-import { ARM7CPU } from '../cpu';
-import * as t from './thumb/thumb';
-
-export type opHandler = (cpu: ARM7CPU) => void;
-
-
-// If you're reading this, you haven't generated the thumb opcode table.
-export const thumbOpTable: StaticArray<StaticArray<opHandler | null>> = []
